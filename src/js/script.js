e0b2aa1b55c4f76a42e99e3d8a80b94541ddd3c3
swedishsailor <kzygowski7@gmail.com>
2021-05-20 12:53:00 +0200

    need to filter buttons in accordion

--- src/js/script.js
+++ src/js/script.js
@@ -83,16 +83,30 @@
       const thisProduct = this;
 
       /*find clickable trigger */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+     
+      console.log(clickableTrigger);
 
       /*START: add event listener to clickable trigger on event click*/
+      clickableTrigger.addEventListener('click', function(event){
 
       /* prevent default action for event */
+      event.preventDefault();
 
       /* find active product (product that has active class */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
 
       /* if there is active product and its not thisProduct.element, remove class active from it */
+      if(activeProduct != null && activeProduct != thisProduct.element){
+        thisProduct.element.classList.remove('active');
+      }
 
       /* toggle active class on thisProduct.element */
+      //for (let element in thisProduct.element){
+      thisProduct.element.classList.toggle('active');
+     // }
+      
+      });
     }
   }
 
