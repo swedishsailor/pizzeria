e7bfd05171af45cc8e17aed1e0bacd3ea26767f4
swedishsailor <kzygowski7@gmail.com>
2021-06-28 18:07:30 +0200

    date and hour picker in project!

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,68 +1,69 @@
 import { settings, select } from './settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
+class AmountWidget extends BaseWidget {
+  constructor(element) {
+    super(element, settings.amountWidget.defaultValue);
+    const thisWidget = this;
 
-      thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value);
-      thisWidget.initActions(settings.amountWidget.defaultValue);
-
-
-    }
-
-    getElements(element) {
-      const thisWidget = this;
-  
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    }
-
-    setValue(value) {
-      const thisWidget = this;
-
-      const newValue = parseInt(value);
-
-      /* To Do, add validation */
-
-      if (settings.amountWidget.defaultValue !== newValue && !isNaN(newValue) && !(newValue < settings.amountWidget.defaultMin) && !(newValue > settings.amountWidget.defaultMax)) {
-        settings.amountWidget.defaultValue = newValue;
-        thisWidget.announce(settings.amountWidget.defaultValue);
-      }
-      thisWidget.input.value = settings.amountWidget.defaultValue;
-
-    }
-
-    initActions() {
-      const thisWidget = this;
+    thisWidget.getElements(element);
 
 
-      thisWidget.input.addEventListener('change', function () {
-        thisWidget.setValue(thisWidget.input.value);
-      });
-      thisWidget.linkDecrease.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisWidget.setValue(settings.amountWidget.defaultValue - 1);
-        thisWidget.announce(settings.amountWidget.defaultValue);
-      });
-      thisWidget.linkIncrease.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisWidget.setValue(settings.amountWidget.defaultValue + 1);
-      });
+    thisWidget.initActions();
 
-    }
+    //console.log('AmountWidget: ', thisWidget);
 
-    announce() {
-      const thisWidget = this;
 
-      const event = new CustomEvent('updated', {
-        bubbles: true
-      });
-      thisWidget.element.dispatchEvent(event);
-
-    }
   }
 
-  export default AmountWidget;
+  getElements() {
+    const thisWidget = this;
+  
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
+  }
+
+  isValid(value) {
+    return !isNaN(value) &&
+      value >= settings.amountWidget.defaultMin &&
+      value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
+  initActions() {
+    const thisWidget = this;
+
+
+    thisWidget.dom.input.addEventListener('change', function () {
+      //thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+
+  }
+
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event);
+
+  }
+}
+
+export default AmountWidget;
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,37 +1,55 @@
 import { templates, select } from './settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
-    constructor(element){
+    constructor(wrapper){
         const thisBooking = this;
-        thisBooking.widgetContainer = element;
-        thisBooking.render(element);
+       // thisBooking.widgetContainer = wrapper;
+        thisBooking.render(wrapper);
         thisBooking.initWidgets();
        
     }
 
-    render(element) {
+    render(wrapper) {
         const thisBooking = this;
         const generatedHTML = templates.bookingWidget();
         thisBooking.dom = {};
-        thisBooking.dom.wrapper = element;
-        console.log(thisBooking.dom.wrapper, 'HTML:', generatedHTML);
+        thisBooking.dom.wrapper = wrapper;
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+        //console.log(thisBooking.dom.wrapper, 'HTML:', generatedHTML);
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
-        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+        
+        
 
     }
 
     initWidgets(){
         const thisBooking = this;
-        thisBooking.peopleAmount = new AmountWidget (thisBooking.dom.peopleAmount);
-        thisBooking.dom.peopleAmount.addEventListener('click', function(){});
+        thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.dom.peopleAmount.addEventListener('click', function(){
+            console.log('pplAMount');
+        });
 
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
-        thisBooking.dom.hoursAmount.addEventListener('click', function(){});
+        thisBooking.dom.hoursAmount.addEventListener('click', function(){
+            console.log('hourAmount');
+        });
 
-        
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.dom.datePicker.addEventListener('click', function(){
+            console.log('datepicker');
+        });
+
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+        thisBooking.dom.hourPicker.addEventListener('click',function(){
+            console.log('hourpicker');
+        });
     }
 }
 
