e050b9b48b7345f20225619a13fc13c05cc1bb18
Kamil Zygowski <kzygowski7@gmail.com>
2021-06-29 15:45:00 +0200

    little update

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -63,10 +63,17 @@ class Booking{
             ]);
         })
         .then(function([bookings, eventsCurrent, eventsRepeat]){
-            console.log(bookings);
-            console.log(eventsCurrent);
-            console.log(eventsRepeat);
+            //console.log(bookings);
+            //console.log(eventsCurrent);
+            //console.log(eventsRepeat);
+            thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
         });
+    }
+
+    parseData(bookings, eventsCurrent, eventsRepeat){
+        const thisBooking = this;
+
+        
     }
 
     render(wrapper) {
